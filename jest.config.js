8b31bb8bd4b2bf732173e22a4c2b8fa86817a6d2
guilhermesmatiola <guimatiola@gmail.com>
2022-09-25 11:02:33 -0300

    fix in jest config

--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,16 @@ export default {
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
+  coveragePathIgnorePatterns:[
+    "node_modules",
+    "test-config",
+    "interfaces",
+    "repositories",
+    "jestGlobalMocks.ts",
+    "<rootDir>/src/server.ts",
+    "<rootDir>/src/database.ts",
+    "<rootDir>/src/utils",
+    "<rootDir>/src/config",
+    "<rootDir>/tests/factories",
+  ]
 };
